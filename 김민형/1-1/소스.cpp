a3a2be1e86cbd927abc147e37d4825a50f8ac702
민형 김 <spawng@naver.com>
2022-09-13 16:54:54 +0900

    김민형 1-1

---
+++ 김민형/1-1/소스.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "ÀÎÆ®¾ÆÀÌ ±è¹ÎÇü" << endl;
+
+	return 0;
+}
+/*
+int main()
+{
+	int a = 0;
+
+	cin >> a;
+	cout << a << endl;
+	return 0;
+}
+*/
