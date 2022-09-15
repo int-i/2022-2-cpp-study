db5581f9fee7e01ad22f1c8b6c7d4397acf29673
전동재 <jys486077@naver.com>
2022-09-15 21:22:27 +0900

    전동재 1-3

---
+++ 전동재/1-3/1-3.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N, X;
+	cin >> N >> X;
+	int num[1000];
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num[i];
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		if (num[i] < X)
+		{
+			cout << num[i] << " ";
+		}
+	}
+}
