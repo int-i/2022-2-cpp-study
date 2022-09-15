0208201c9f01750bcdf078608b778c120249ef89
jawa9635 <99813596+jawa9635@users.noreply.github.com>
2022-09-15 22:48:11 +0900

    장채영 1-3

---
+++ 장채영/1-3/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+
+	cin >> a;
+	cin	>> b;
+
+	for (int i = 0; i < a; i++) 
+	{
+		cin >> c;
+		if ( c < b) 
+		{
+			cout << c;
+		}
+	}
+
+
+}
