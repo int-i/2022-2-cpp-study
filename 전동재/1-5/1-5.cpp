3af5c802578de2dd2c1a1c9354348e3ecd67b995
전동재 <jys486077@naver.com>
2022-09-16 22:02:04 +0900

    전동재 1-4,1-5

---
+++ 전동재/1-5/1-5.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int subject;
+
+	cin >> subject;
+	int score[1000] = { };
+	for (int i = 0; i < subject; i++)
+	{
+		cin >> score[i];
+	}
+	float max = score[0];
+	
+	for (int i = 0; i < subject; i++)
+	{
+		if (max < score[i])
+		{
+			max = score[i];
+		}
+	}
+	//cout << "max=" << max << endl;
+	float joojak[1000] = { };
+	float sum = 0;
+	for (int i = 0; i < subject; i++)
+	{
+		joojak[i] = score[i]/max * 100;
+		//cout << "joojak:" << joojak[i];
+		sum = sum + joojak[i];
+	}
+	cout << sum/subject;
+}
